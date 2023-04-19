27e30635206813bf815dcb6ec4c2088d04f46139
Nicolas <nicolasgabriel.developer@gmail.com>
2023-04-18 23:12:54 -0300

    FEAT: CarrosselDeImagens.vue
    Criando componente que ficará no topo da homeview

---
+++ src/components/CarrosselDeImagens.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-carrossel"> 
+  <h1> Imagens aqui</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Carrossel-de-Imagens',
+});
+</script>
+
+<style scoped>
+.container-carrossel{
+  width: 100%;
+  height: 35vh;
+  background-color: aliceblue;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="principal-container">
+  <div>
+    <CarrosselDeImagens/>
+  </div>
     <!-- <ul>
       <li v-for="user in users" :key="user.id">{{ user.name }}</li>
     </ul> -->
@@ -7,6 +10,7 @@
 </template>
 
 <script lang="ts">
+import CarrosselDeImagens from '@/components/CarrosselDeImagens.vue';
 import { defineComponent, ref, onMounted } from 'vue';
 
 interface User {
@@ -15,18 +19,17 @@ interface User {
 }
 
 export default defineComponent({
-  name: 'UserList',
-  setup() {
-    const users = ref<User[]>([]);
-
-    onMounted(async () => {
-      const response = await fetch('https://api.themoviedb.org/3/movie/4564?api_key=ea50df2fafdaa8c0f5c42dfbb1bd82f9');
-
-      users.value = await response.json();
-    });
-    console.log(users)
-    return { users };
-  },
+    name: "UserList",
+    setup() {
+        const users = ref<User[]>([]);
+        onMounted(async () => {
+            const response = await fetch("https://api.themoviedb.org/3/movie/4564?api_key=ea50df2fafdaa8c0f5c42dfbb1bd82f9");
+            users.value = await response.json();
+        });
+        console.log(users);
+        return { users };
+    },
+    components: { CarrosselDeImagens }
 });
 
 </script>
