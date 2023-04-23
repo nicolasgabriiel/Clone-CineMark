a4b6496a4512c84d40faf9ba7a3014853ffb78dd
Nicolas <nicolasgabriel.developer@gmail.com>
2023-04-23 14:42:30 -0300

    CHORE: FilmesEmCartaz
    Corrigindo problema com código e acertando as pontas soltas que faltavam no commit anterior

--- src/components/FilmesEmCartaz.vue
+++ src/components/FilmesEmCartaz.vue
@@ -1,7 +1,7 @@
 <template>
     <h1>Filmes em Cartaz</h1>
     <div class="box">
-        <div class="container-filmes" v-for=" (filme, index) in FilmesFiltrados" :key="filme.id"  :class="identificador[index]">
+        <div class="container-filmes" v-for=" filme in FilmesFiltrados" :key="filme.id" :class="filme.identificador">
             <div class="imagem" :style="{ backgroundImage: 'url(' + filme.url + ')' }"></div>
             <h2 class="nome">{{ filme.nome }}</h2>
         </div>
@@ -11,6 +11,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import axios from 'axios';
+
+interface Filme {
+    id: number;
+    nome: string;
+    url: string;
+    identificador: number;
+}
+
+
 export default defineComponent({
     name: 'Filmes-em-cartaz',
     async mounted() {
@@ -18,12 +27,12 @@ export default defineComponent({
             const listaIdsFilmes = [502356, 594767, 603692, 758323, 804150, 493529, 1008005]
             for (let i = 0; i < 7; i++) {
                 const response = await axios.get(`https://api.themoviedb.org/3/movie/${listaIdsFilmes[i]}?api_key=${this.key}`);
-                this.identificador[i] = i
                 this.filmes[i] = {
-                id: response.data.id,
-                nome: response.data.original_title,
-                url: this.buscadorDeImagem + response.data.poster_path,
-        };
+                    id: response.data.id,
+                    nome: response.data.original_title,
+                    url: this.buscadorDeImagem + response.data.poster_path,
+                    identificador: i
+                };
             }
             console.log(this.filmes)
         } catch (error) {
@@ -34,24 +43,25 @@ export default defineComponent({
         return {
             buscadorDeImagem: 'http://image.tmdb.org/t/p/original',
             key: 'ea50df2fafdaa8c0f5c42dfbb1bd82f9',
-            filmes: [] as { id: number, nome: string, url: string }[],
+            filmes: [] as { id: number, nome: string, url: string, identificador: number }[],
             mostrar: [true, false, false, false, false, false, false],
-            identificador: [] as number[]
         }
     },
     computed: {
-    FilmesFiltrados() {
-      return this.filmes.filter((_, index) => this.mostrar[index]);
-    },
-}
+        FilmesFiltrados(): Filme[] {
+            return this.filmes.filter((filme: Filme, index: number) => this.mostrar[index]);
+        },
+
+    }
 })
 </script>
 
 <style scoped>
-.box{
+.box {
     display: flex;
     flex-direction: row;
 }
+
 .container-filmes {
     display: flex;
     flex-direction: column;
@@ -67,7 +77,8 @@ export default defineComponent({
     font-size: 20px;
     color: black
 }
-.hide{
+
+.hide {
     display: none !important;
 }
 </style>
